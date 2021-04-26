c1cdc9fdc58bcce0cd1d29e942173fb1aa9d45f6
Tai-wei Sun <ghnbsun2011@gmail.com>
2021-04-26 10:05:26 +0800

    test char*

---
+++ c/tmp027.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main (void) {
+	char *kkk;
+
+	kkk="123";
+	printf("kkk1=%s\n",kkk);
+	kkk="456";
+	printf("kkk2=%s\n",kkk);
+	kkk="789";
+	printf("kkk3=%s\n",kkk);
+
+	return(0);
+}
+
+
+
+
